c3375e6ae158d4a2d76d0170ea3d64de2ebced33
Jérôme Mulsant <jerome@rue-de-la-vieille.fr>
2023-01-26 17:58:50 +0100

    Correction on Reply-To header and error logging

--- src/Message.php
+++ src/Message.php
@@ -53,13 +53,20 @@ class Message extends PHPMailer
                 'To' => array_map([$this, 'format_address'], $this->to),
                 'Cc' => array_map([$this, 'format_address'], $this->cc),
                 'Bcc' => array_map([$this, 'format_address'], $this->bcc),
-                'ReplyTo' => array_map([$this, 'format_address'], $this->ReplyTo),
                 'Subject' => $this->Subject,
                 'ContentType' => $this->ContentType,
                 'Attachments' => $this->format_attachments('attachment'),
                 'InlinedAttachments' => $this->format_attachments('inline'),
             ]
         );
+
+        if ($this->ReplyTo) {
+            $replyToHeaders = array_values($this->ReplyTo);
+            if (count($replyToHeaders) > 1) {
+                trigger_error('Only a single ReplyTo header is supported', E_USER_WARNING);
+            }
+            $message['ReplyTo'] = $this->format_address($replyToHeaders[0]);
+        }
 
         $message[$this->ContentType === PHPMailer::CONTENT_TYPE_TEXT_HTML ? 'HTMLPart' : 'TextPart'] = $this->Body;
 
@@ -73,14 +80,8 @@ class Message extends PHPMailer
             return true;
         }
 
-        $data = $response->getData();
-        throw new \PHPMailer\PHPMailer\Exception(
-            sprintf(
-                '%s (error related to: %s)',
-                $data['ErrorMessage'],
-                implode(', ', $data['ErrorRelatedTo'])
-            ),
-            $data['StatusCode']
-        );
+        $error = json_encode(['response' => $response->getBody(), 'message' => $message]);
+        trigger_error($error, E_USER_WARNING);
+        throw new \PHPMailer\PHPMailer\Exception(sprintf('MailJet Error: %s', $error));
     }
 }
